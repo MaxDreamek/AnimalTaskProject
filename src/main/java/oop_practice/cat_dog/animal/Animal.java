30e053344d95a871cc0d7ede1432a57de2afc8bb
Max Styosov <avardn@gmail.com>
2023-10-22 23:22:30 +0300

    Fixed homework

--- src/main/java/oop_practice/cat_dog/animal/Animal.java
+++ src/main/java/oop_practice/cat_dog/animal/Animal.java
@@ -4,16 +4,12 @@ public abstract class Animal {
     private String name;
 
     public Animal(String name) {
-        setName(name);
+        this.name = name;
     }
 
     public abstract void sound();
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
